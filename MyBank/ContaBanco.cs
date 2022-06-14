0212d1225dd1a5be432a662c8a0708dd2556b46a
rnmartins <renansmartins@live.com>
2022-06-14 18:50:35 -0300

    Adicionar arquivos de projeto.

---
+++ MyBank/ContaBanco.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBank
+{
+    public class ContaBanco
+    {
+        public string Numero { get; }
+        public string DonoDaConta { get; set; }
+        public decimal Saldo { 
+            get
+            {
+                decimal saldo = 0;
+                foreach (var item in todasTransacoes)
+                {
+                    saldo += item.Valor;
+                }
+                return saldo;
+            } 
+        }
+
+        public static int numeroConta = 12345;
+
+        private List<Transacao> todasTransacoes = new List<Transacao>();
+
+        public ContaBanco(string nome, decimal valor)
+        {
+            this.DonoDaConta = nome;
+            this.Saldo = valor;
+            numeroConta++;
+            this.Numero = numeroConta.ToString();
+        }
+
+        public void Depositar()
+        {
+
+        }
+
+        public void Sacar()
+        {
+
+        }
+    }
+}
---
+++ MyBank/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBank.ContaBanco conta1 = new ContaBanco("Renan Martins", 1200);
+
+            Console.WriteLine($"A conta {conta1.Numero} de {conta1.DonoDaConta} foi criada com R${conta1.Saldo} de saldo!");
+        }
+    }
+}
