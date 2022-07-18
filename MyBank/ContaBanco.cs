bbfc5ee19416e766888b2862c0346559891a3f0b
rnmartins <renansmartins@live.com>
2022-07-18 12:45:26 -0300

    Atualização do projeto

--- MyBank/ContaBanco.cs
+++ MyBank/ContaBanco.cs
@@ -29,19 +29,50 @@ namespace MyBank
         public ContaBanco(string nome, decimal valor)
         {
             this.DonoDaConta = nome;
-            this.Saldo = valor;
             numeroConta++;
             this.Numero = numeroConta.ToString();
+            Depositar(valor, DateTime.Now, "Saldo inicial");
         }
 
-        public void Depositar()
+        public void Depositar(decimal valor, DateTime data, string obs)
         {
+            if(valor <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(valor), "Não aceitamos depósito de valor 0 ou negativo");
+            }
 
+            Transacao transacao = new Transacao(valor, data, obs);
+            todasTransacoes.Add(transacao);
         }
 
-        public void Sacar()
+        public void Sacar(decimal valor, DateTime data, string obs)
         {
+            if (valor <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(valor), "Não aceitamos saques de valor 0 ou negativo");
+            }
 
+            if (Saldo - valor < 0)
+            {
+                throw new InvalidOperationException("Essa operação não é permitida!");
+            }
+
+            Transacao transacao = new Transacao(-valor, data, obs);
+            todasTransacoes.Add(transacao);
+        }
+
+        public string PegarMovimentacao()
+        {
+            var movimentacoes = new StringBuilder();
+
+            movimentacoes.AppendLine("Data\t\t Valor\t Obs");
+
+            foreach (var item in todasTransacoes)
+            {
+                movimentacoes.AppendLine($"{item.Data.ToShortDateString()}\t {item.Valor}\t {item.Obs}");
+            }
+
+            return movimentacoes.ToString();
         }
     }
 }
--- MyBank/Program.cs
+++ MyBank/Program.cs
@@ -9,6 +9,32 @@ namespace MyBank
             MyBank.ContaBanco conta1 = new ContaBanco("Renan Martins", 1200);
 
             Console.WriteLine($"A conta {conta1.Numero} de {conta1.DonoDaConta} foi criada com R${conta1.Saldo} de saldo!");
+
+            try
+            {
+                conta1.Depositar(100, DateTime.Now, "Deposito");
+                Console.WriteLine($"A conta com R${conta1.Saldo} de saldo!");
+
+                conta1.Sacar(5000, DateTime.Now, "Saque");
+                Console.WriteLine($"A conta com R${conta1.Saldo} de saldo!");
+
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Operação não realizada!");
+            }
+
+            conta1.Sacar(20, DateTime.Now, "Saque");
+            Console.WriteLine($"A conta com R${conta1.Saldo} de saldo!");
+
+            conta1.Sacar(300, DateTime.Now, "Saque");
+            Console.WriteLine($"A conta com R${conta1.Saldo} de saldo!");
+
+            Console.WriteLine(conta1.PegarMovimentacao());
         }
     }
 }
